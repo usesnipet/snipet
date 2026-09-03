39146a5798a3005f0c346f5c044076001b3ea46f
Mayron <mayron.fernandes@alghorit.com.br>
2026-09-03 16:49:15 -0300

    feat: add badge color to llm provider catalog card and avatar to render provider icon

--- web/src/features/llm-provider/components/llm-provider-catalog-card.tsx
+++ web/src/features/llm-provider/components/llm-provider-catalog-card.tsx
@@ -1,6 +1,8 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import stc from "string-to-color";
 
 import { useLlmProviderRegistry } from "../hooks";
 
@@ -12,17 +14,29 @@ export type LLMProviderCatalogCardProps = {
 export function LLMProviderCatalogCard({ provider }: LLMProviderCatalogCardProps) {
   const { data } = useLlmProviderRegistry();
   const registry = data?.find(r => r.key === provider.provider)
+  const color = stc(provider.provider)
+  console.log(color);
 
   return (
     <Card
       className={cn("flex h-full flex-col")}
     >
       <CardHeader className="flex flex-row items-start gap-3 space-y-0 pb-3">
-        <img src={registry?.icon} alt="provider icon" />
+        <Avatar>
+          <AvatarImage
+            src={registry?.icon}
+            alt={registry?.icon}
+            className={cn(!provider.enabled && "grayscale")}
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
         <div className="flex min-w-0 flex-1 items-center justify-between space-y-1">
           <div className="flex min-w-0 flex-wrap items-center gap-2">
             <h2 className="truncate text-base font-semibold leading-tight">{provider.name}</h2>
-            <Badge variant="secondary" className="shrink-0 font-normal">
+            <Badge variant="secondary" className="shrink-0 font-normal gap-2">
+              <div style={{ background: color }} className="w-2 h-2 rounded-full">
+
+              </div>
               {provider.provider}
             </Badge>
           </div>
